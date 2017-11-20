2c4d5288e34c54d8b294ee40db20a500673464d8
Grzesiek <jakubiak-grzegorz@wp.pl>
2017-11-21 00:19:21 +0100

    dodanie aktwnosci do logowania

--- app/src/main/java/com/pracainz20/ChooseProfilActivity.java
+++ app/src/main/java/com/pracainz20/ChooseProfilActivity.java
@@ -1,15 +1,43 @@
 package com.pracainz20;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class ChooseProfilActivity extends AppCompatActivity {
+
+    Button trainee_button;
+    Button amateur_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_profil);
+
+
+
+        trainee_button = (Button) findViewById(R.id.trainee_button);
+        amateur_button = (Button) findViewById(R.id.amateur_button);
+
+        trainee_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
+
+        amateur_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
     }
+
+
 
 
 }
