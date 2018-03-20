d554bc085e5407197cf345c50014bb7b556443fd
Grzesiek <jakubiak-grzegorz@wp.pl>
2018-03-20 15:22:01 +0100

    wysylanie zaproszen i przypomnien

--- app/src/main/java/com/pracainz20/Activity/MateProfileActivity.java
+++ app/src/main/java/com/pracainz20/Activity/MateProfileActivity.java
@@ -43,6 +43,7 @@ public class MateProfileActivity  extends AppCompatActivity
 
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
